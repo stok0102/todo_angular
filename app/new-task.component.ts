27dd6b6cda53bba3cbdd8699ca686ce42f3c9a28
Caleb Stokka <caleb+leah@>
2016-08-08 14:37:05 -0700

    add priorities

--- app/new-task.component.ts
+++ app/new-task.component.ts
@@ -8,17 +8,25 @@ import { Task } from './task.model';
     <div class="task-form">
       <h3>Create Task:</h3>
       <input placeholder="Description" class="col-sm-8 input-lg" #newDescription>
-      <button (click)="addTask(newDescription)" class="btn-success btn-lg add-button">Add</button>
+      <select placeholder="Priority" #newPriority id="new-priority">
+        <option value="high">High</option>
+        <option value="medium">Medium</option>
+        <option value="low" selected="selected">Low</option>
+      </select>
+      <button (click)="addTask(newDescription, newPriority)" class="btn-success btn-lg add-button">Add</button>
     </div>
   `
 })
 export class NewTaskComponent {
-  public onSubmitNewTask: EventEmitter<String>;
+  public onSubmitNewTask: EventEmitter<Object>;
   constructor(){
     this.onSubmitNewTask = new EventEmitter();
   }
-  addTask(userDescription: HTMLInputElement){
-    this.onSubmitNewTask.emit(userDescription.value);
+  addTask(userDescription: HTMLInputElement, userPriority: HTMLSelectElement){
+    this.onSubmitNewTask.emit({
+      description: userDescription.value,
+      priority: userPriority.value
+    });
     userDescription.value = "";
   }
 }
